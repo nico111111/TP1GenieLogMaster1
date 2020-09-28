94c6c81cbf80266df05433cdce08940a6197a9c0
febeaudoin <febeaudoin@turing.local.isima.fr>
2020-09-28 12:52:15 +0200

    tab en espace, final, static etc... dans GildedRoseTest.java

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -5,168 +5,168 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 class GildedRoseTest {
-	String normal = "Anneau de force du Dragon +5";
-	String brie = "Aged Brie";
-	String backStage = "Backstage passes to a TAFKAL80ETC concert";
-	String conjured = "Conjured Mana Cake";
-	String sulfuras = "Sulfuras, Hand of Ragnaros";
+  static String normal = "Anneau de force du Dragon +5";
+  static String brie = "Aged Brie";
+  static String backStage = "Backstage passes to a TAFKAL80ETC concert";
+  static String conjured = "Conjured Mana Cake";
+  static String sulfuras = "Sulfuras, Hand of Ragnaros";
 
   @Test
   void testItemNormalDecaySellIn() {
-    Item[] items = new Item[] { new Item(normal, 5, 20) };
-    GildedRose app = new GildedRose(items);
+    final Item[] items = new Item[] {new Item(normal, 5, 20) };
+    final GildedRose app = new GildedRose(items);
     app.updateQuality();
     assertThat(app.getItems()[0].sellIn, is(4));
   }
-	
-	@Test
-	void testItemNormalDecayQuality() {
-    Item[] items = new Item[] { new Item(normal, 5, 20) };
-    GildedRose app = new GildedRose(items);
+
+  @Test
+  void testItemNormalDecayQuality() {
+    final Item[] items = new Item[] {new Item(normal, 5, 20) };
+    final GildedRose app = new GildedRose(items);
     app.updateQuality();
     assertThat(app.getItems()[0].quality, is(19));
   }
 
-	@Test
-	void testItemEndOfDateDecayQuality() {
-    Item[] items = new Item[] { new Item(normal, 0, 20) };
-    GildedRose app = new GildedRose(items);
+  @Test
+  void testItemEndOfDateDecayQuality() {
+    final Item[] items = new Item[] {new Item(normal, 0, 20) };
+    final GildedRose app = new GildedRose(items);
     app.updateQuality();
     assertThat(app.getItems()[0].quality, is(18));
   }
 
-	@Test
-	void testQualityNotNegative() {
-      Item[] items = new Item[] { new Item(normal, 0, 0) };
-      GildedRose app = new GildedRose(items);
-      app.updateQuality();
-      assertThat(app.getItems()[0].quality, is(0));
-  }
-
-	@Test
-	void testQualityNotNegativeAgainIf1AndPerimed() {
-    Item[] items = new Item[] { new Item(normal, 0, 1) };
-    GildedRose app = new GildedRose(items);
+  @Test
+  void testQualityNotNegative() {
+    final Item[] items = new Item[] {new Item(normal, 0, 0) };
+    final GildedRose app = new GildedRose(items);
     app.updateQuality();
     assertThat(app.getItems()[0].quality, is(0));
   }
 
-	@Test
-	void testAgedBrieQualityAugment() {
-    Item[] items = new Item[] { new Item(brie, 5, 5) };
-    GildedRose app = new GildedRose(items);
+  @Test
+  void testQualityNotNegativeAgainIf1AndPerimed() {
+    final Item[] items = new Item[] {new Item(normal, 0, 1) };
+    final GildedRose app = new GildedRose(items);
+    app.updateQuality();
+    assertThat(app.getItems()[0].quality, is(0));
+  }
+
+  @Test
+  void testAgedBrieQualityAugment() {
+    final Item[] items = new Item[] {new Item(brie, 5, 5) };
+    final GildedRose app = new GildedRose(items);
     app.updateQuality();
     assertThat(app.getItems()[0].quality, is(6));
   }
-	
-	@Test
-	void testAgedBrieQualityNoMoreThan50() {
-    Item[] items = new Item[] { new Item(brie, 5, 50) };
-    GildedRose app = new GildedRose(items);
+
+  @Test
+  void testAgedBrieQualityNoMoreThan50() {
+    final Item[] items = new Item[] {new Item(brie, 5, 50) };
+    final GildedRose app = new GildedRose(items);
     app.updateQuality();
     assertThat(app.getItems()[0].quality, is(50));
   }
 
-	@Test
-	void testAgedBrieQualityDoubleWhen0() {
-    Item[] items = new Item[] { new Item(brie, 0, 12) };
-    GildedRose app = new GildedRose(items);
+  @Test
+  void testAgedBrieQualityDoubleWhen0() {
+    final Item[] items = new Item[] {new Item(brie, 0, 12) };
+    final GildedRose app = new GildedRose(items);
     app.updateQuality();
     assertThat(app.getItems()[0].quality, is(14));
   }
-    
+
   @Test
-	void testAgedBrieQualityNoMoreThan50Perimed() {
-    Item[] items = new Item[] { new Item(brie, 0, 49) };
-    GildedRose app = new GildedRose(items);
+  void testAgedBrieQualityNoMoreThan50Perimed() {
+    final Item[] items = new Item[] {new Item(brie, 0, 49) };
+    final GildedRose app = new GildedRose(items);
     app.updateQuality();
     assertThat(app.getItems()[0].quality, is(50));
   }
 
-	@Test
-	void testSulfurasQuality() {
-    Item[] items = new Item[] { new Item(sulfuras, 0, 80) };
-    GildedRose app = new GildedRose(items);
+  @Test
+  void testSulfurasQuality() {
+    final Item[] items = new Item[] {new Item(sulfuras, 0, 80) };
+    final GildedRose app = new GildedRose(items);
     app.updateQuality();
     assertThat(app.getItems()[0].quality, is(80));
   }
 
-	@Test
-	void testPassQualityMoreThanTen() {
-    Item[] items = new Item[] { new Item(backStage, 20, 15) };
-    GildedRose app = new GildedRose(items);
+  @Test
+  void testPassQualityMoreThanTen() {
+    final Item[] items = new Item[] {new Item(backStage, 20, 15) };
+    final GildedRose app = new GildedRose(items);
     app.updateQuality();
     assertThat(app.getItems()[0].quality, is(16));
   }
 
-	@Test
-	void testPassQualityLessThanTenMoreThanFive() {
-    Item[] items = new Item[] { new Item(backStage, 10, 15) };
-    GildedRose app = new GildedRose(items);
+  @Test
+  void testPassQualityLessThanTenMoreThanFive() {
+    final Item[] items = new Item[] {new Item(backStage, 10, 15) };
+    final GildedRose app = new GildedRose(items);
     app.updateQuality();
     assertThat(app.getItems()[0].quality, is(17));
   }
 
-	@Test
-	void testPassQualityLessThanFive() {
-    Item[] items = new Item[] { new Item(backStage, 5, 15) };
-    GildedRose app = new GildedRose(items);
+  @Test
+  void testPassQualityLessThanFive() {
+    final Item[] items = new Item[] {new Item(backStage, 5, 15) };
+    final GildedRose app = new GildedRose(items);
     app.updateQuality();
     assertThat(app.getItems()[0].quality, is(18));
   }
 
-	@Test
-	void testPassQualityZeroSellInZero() {
-    Item[] items = new Item[] { new Item(backStage, 0, 15) };
-    GildedRose app = new GildedRose(items);
+  @Test
+  void testPassQualityZeroSellInZero() {
+    final Item[] items = new Item[] {new Item(backStage, 0, 15) };
+    final GildedRose app = new GildedRose(items);
     app.updateQuality();
     assertThat(app.getItems()[0].quality, is(0));
   }
 
-	@Test
-	void testPassQualityNoMoreThanFifty() {
-    Item[] items = new Item[] { new Item(backStage, 4, 49) };
-    GildedRose app = new GildedRose(items);
+  @Test
+  void testPassQualityNoMoreThanFifty() {
+    final Item[] items = new Item[] {new Item(backStage, 4, 49) };
+    final GildedRose app = new GildedRose(items);
     app.updateQuality();
     assertThat(app.getItems()[0].quality, is(50));
   }
 
-	@Test
-	void testPassQualityMoreThanFifty() {
-    Item[] items = new Item[] { new Item(backStage, 4, 55) };
-    GildedRose app = new GildedRose(items);
+  @Test
+  void testPassQualityMoreThanFifty() {
+    final Item[] items = new Item[] {new Item(backStage, 4, 55) };
+    final GildedRose app = new GildedRose(items);
     app.updateQuality();
     assertThat(app.getItems()[0].quality, is(55));
   }
 
-	@Test
-	void testConjuredQualityNormal() {
-    Item[] items = new Item[] { new Item(conjured, 4, 25) };
-    GildedRose app = new GildedRose(items);
+  @Test
+  void testConjuredQualityNormal() {
+    final Item[] items = new Item[] {new Item(conjured, 4, 25) };
+    final GildedRose app = new GildedRose(items);
     app.updateQuality();
     assertThat(app.getItems()[0].quality, is(23));
   }
 
-	@Test
-	void testConjuredQualityPerimed() {
-    Item[] items = new Item[] { new Item(conjured, 0, 25) };
-    GildedRose app = new GildedRose(items);
+  @Test
+  void testConjuredQualityPerimed() {
+    final Item[] items = new Item[] {new Item(conjured, 0, 25) };
+    final GildedRose app = new GildedRose(items);
     app.updateQuality();
     assertThat(app.getItems()[0].quality, is(21));
   }
 
-	@Test
-	void testConjuredQualityNoLessThan0() {
-    Item[] items = new Item[] { new Item(conjured, 0, 1) };
-    GildedRose app = new GildedRose(items);
+  @Test
+  void testConjuredQualityNoLessThan0() {
+    final Item[] items = new Item[] {new Item(conjured, 0, 1) };
+    final GildedRose app = new GildedRose(items);
     app.updateQuality();
     assertThat(app.getItems()[0].quality, is(0));
   }
 
-	@Test
-	void testConjuredQuality0() {
-    Item[] items = new Item[] { new Item(conjured, 0, 0) };
-    GildedRose app = new GildedRose(items);
+  @Test
+  void testConjuredQuality0() {
+    final Item[] items = new Item[] {new Item(conjured, 0, 0) };
+    final GildedRose app = new GildedRose(items);
     app.updateQuality();
     assertThat(app.getItems()[0].quality, is(0));
   }
